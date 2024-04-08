0627c15cb3e17e9e0ad516c31b4b89f78a4bc9c5
Yirang <olrang.2@gmail.com>
2024-04-08 09:27:11 +0900

    axios setting

---
+++ src/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        {{ testMessage }}
+    </div>
+
+    <script>
+        export default{
+            data(){
+                return{
+                    textMessage:'',
+                };
+            },
+            mounted(){
+                this.$axios.get('http://localhost:8080/recommend/list')
+                .then((response) => {
+                    this.testMessage = response.data;
+                })
+                .catch((error) => {
+                    console.error("There was an error!", error);
+                })
+            },
+        }
+    </script>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from "axios"
 
-createApp(App).mount('#app')
+const app = createApp(App).use(router);
+
+app.use(store).use(router).mount('#app');
+app.config.globalProperites.$axios = axios;
+
+// createApp(App).mount('#app')
