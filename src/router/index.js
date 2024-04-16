09f78965669ff00abfce7c0add151fcf3198cdc5
Ruah Kim <kimhk0315@gmail.com>
2024-04-16 17:28:33 +0900

    🎨 머지 커밋

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ReviewBoard from '../views/Review/ReviewBoard.vue'
 import LogIn from '../views/Login/LogIn.vue'
-
-
+import ChattingView from '../views/Chat/ChattingView.vue'
+import ChatRoom from '../views/Chat/ChatRoom.vue';
+import BudgetView from '../views/Budget/BudgetView.vue';
 
 
 const routes = [
@@ -21,6 +22,22 @@ const routes = [
     path: '/login',
     name: 'login',
     component: LogIn
+  },
+  {
+    path: '/chatting',
+    name: 'chatting',
+    component: ChattingView
+  },
+  {
+    path: '/chat/room/:roomId', // 동적 세그먼트를 사용한 경로
+    name: 'ChatRoom',
+    component: ChatRoom,
+    props: true // 컴포넌트에 URL 파라미터를 props로 전달
+  },
+  {
+    path: '/budget',
+    name: 'budget',
+    component: BudgetView
   },
 ]
 
