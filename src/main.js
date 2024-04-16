fa11cd54a2d35a4d50a286746b213bdbdb526c4e
DESKTOP-D524MI3\804-07 <bae4445@gmail.com>
2024-04-16 11:16:43 +0900

    mergy login+review

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ app.use(store).use(router).mount('#app');
 app.component('font-awesome-icon', FontAwesomeIcon)
 
 
-// axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'http://localhost:8080';
 axios.interceptors.request.use(function (config) {
     const token = localStorage.getItem('Authorization');
     if (token) {
