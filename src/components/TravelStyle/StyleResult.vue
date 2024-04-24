c6651079dfbb6c46e0f021edba891b58f581f17e
배정현\bjh73 <bae4445@gmail.com>
2024-04-25 00:56:26 +0900

    [Feature] MBTI 여행스타일 화면개발 완료

--- src/components/TravelStyle/StyleResult.vue
+++ src/components/TravelStyle/StyleResult.vue
@@ -1,17 +1,29 @@
 
 <template>
     <div class="survey-container" v-if="results">
-      <p class="mbtiType">{{ mbtiType }} : {{ mbtiProfile.style }}</p>
-      <img :src="mbtiProfile.imageUrl" alt="MBTI Image" />
-      <p>{{ mbtiProfile.description }}</p>
-      <p>{{ mbtiProfile.description2 }}</p>
-      <p>{{ mbtiProfile.description3 }}</p>
-      <p>{{ mbtiProfile.description4 }}</p>
-      <p>{{ mbtiProfile.description5 }}</p>
-      <p>{{ mbtiProfile.description6 }}</p>
-      <p>{{ mbtiProfile.description7 }}</p>
-      <p>{{ mbtiProfile.soulMate }}</p>
-      <p>{{ mbtiProfile.hateMate }}</p>
+      <div class="borderLine">
+      <img :src="mbtiProfile.imageUrl" alt="MBTI Image" class="mbtiImage"/>
+      <div class="styleModal">
+      <p class="mbtiType">{{ mbtiType }} <br/> {{ mbtiProfile.style }}</p>
+      <div class="mbtiProfile-description">
+      <p><font-awesome-icon icon="fa-solid fa-plane" />  {{ mbtiProfile.description }}</p>
+      <p><font-awesome-icon icon="fa-solid fa-plane" />  {{ mbtiProfile.description2 }}</p>
+      <p><font-awesome-icon icon="fa-solid fa-plane" />  {{ mbtiProfile.description3 }}</p>
+      <p><font-awesome-icon icon="fa-solid fa-plane" />  {{ mbtiProfile.description4 }}</p>
+      <p><font-awesome-icon icon="fa-solid fa-plane" />  {{ mbtiProfile.description5 }}</p>
+      <p><font-awesome-icon icon="fa-solid fa-plane" />  {{ mbtiProfile.description6 }}</p>
+      <p><font-awesome-icon icon="fa-solid fa-plane" />  {{ mbtiProfile.description7 }}</p>
+      <p>여행 찰떡 조합 : {{ mbtiProfile.soulMate }}</p>
+      <p>여행 멸망 조합 : {{ mbtiProfile.hateMate }}</p>
+      </div>
+      </div>
+      <router-link to="/traveltest">
+      <button class="restart-button">테스트 다시하기</button><br/>
+      </router-link>
+      <router-link to="/">
+      <button class="home-button">메인페이지로</button>
+      </router-link>
+      </div>
     </div>
     <div v-else>
       <p>결과 데이터를 불러오는데 실패했습니다.</p>
@@ -33,9 +45,9 @@
           description6: "낯선 여행지에 가서도 말이 통하지 않는 현지 분들과 친하게 지내며 모두의 텐션을 업 시켜줍니다.",
           description7: "좋아하는 게 너무 많고 감수성이 풍부하며 다니는 곳마다 감동하고 분위기에 취합니다.",
           imageUrl: require("@/assets/images/ENFJ.png"), // 예시 경로
-          style: "프로여행러",
-          soulMate: "로맨틱여행파(INFP)",
-          hateMate: "프로혼행러(ISTP)"
+          style: "프로 여행러",
+          soulMate: "로맨틱 여행파(INFP)",
+          hateMate: "프로 혼행러(ISTP)"
         },
         "ENFP": {
           description: "무계획 여행이 진정한 여행의 재미! 예상치 못한 여행이 추억이 더 오래 남아요!",
@@ -46,9 +58,9 @@
           description6: "가는 곳마다 SNS용 사진 스팟을 잘 찾으며 여행지에서 처음 보는 사람들과도 금방 친해져요.",
           description7: "남들이 가는 곳이 아닌 새로운 장소나 새로운 코스에 도전하는 스타일입니다.",
           imageUrl: require("@/assets/images/ENFP.png"), // 예시 경로
-          style: "여행무계획러",
-          soulMate: "감성여행자(INFJ)",
-          hateMate: "프로엑셀러(ISTJ)"
+          style: "여행 무계획러",
+          soulMate: "감성 여행자(INFJ)",
+          hateMate: "프로 엑셀러(ISTJ)"
         },
         "ENTJ": {
           description: "다 같이 가는 여행을 위해 상세 날짜나 장소를 투표를 정하고 그 결정 사항에 대한 이의는 없어요.",
@@ -58,9 +70,9 @@
           description5: "늘 여행을 갈 때 나도 모르게 총무 역할을 하거나 리더가 되어있습니다.",
           description6: "에너지 넘치는 모험가 스타일로 험하고 힘든 여행을 해도 어려운 여행을 완수했다는 성취감을 느낍니다.",
           imageUrl: require("@/assets/images/ENTJ.png"), // 예시 경로
-          style: "효율로보트",
-          soulMate: "한적스팟러(INTP)",
-          hateMate: "힐링여행러(ISFP)"
+          style: "효율 로보트",
+          soulMate: "한적 스팟러(INTP)",
+          hateMate: "힐링 여행러(ISFP)"
         },
         "ENTP": {
           description: "늘 똑같은 반복적인 인생의 지루함을 느껴 즉흥적으로 여행 약속을 잡는 경우가 많아요.",
@@ -71,9 +83,9 @@
           description6: "내가 그곳에 꼭 가야 하는 이유가 있어요.",
           description7: "친구들과 여행 계획을 할 때 정말 아니라고 생각하는 곳은 어떻게든 가지 않으려고 해요.",
           imageUrl: require("@/assets/images/ENTP.png"), // 예시 경로
-          style: "위기탈출넘버원",
-          soulMate: "효율계획러(INTJ)",
-          hateMate: "여행한정인싸(ISFJ)"
+          style: "위기 탈출넘버원",
+          soulMate: "효율 계획러(INTJ)",
+          hateMate: "여행 한정인싸(ISFJ)"
         },
         "ESFJ": {
           description: "혹시나 내가 그 장소에 갔는데 그 식당이 맛없을까 봐/숙소에 자리가 없을까 봐 즉흥 여행은 상상할 수 없어요.",
@@ -84,9 +96,9 @@
           description6: "여행 내내 웃음이 사라지지 않고 친구들에게 적극적인 리액션과 호응해 분위기 메이커를 담당해요.",
           description7: "친구들이 다 가고 싶은 여행지로 여행을 계획하기 때문에 단톡방에 투표를 자주 올려요",
           imageUrl: require("@/assets/images/ESFJ.png"), // 예시 경로
-          style: "프로핫스팟러",
-          soulMate: "힐링여행러(ISFP)",
-          hateMate: "한적스팟러(INTP)"
+          style: "프로 핫스팟러",
+          soulMate: "힐링 여행러(ISFP)",
+          hateMate: "한적 스팟러(INTP)"
         },
         "ESFP": {
           description: "여행을 떠날 때 친구와 카톡 하다 갈까? 가볼까? 하며 즉흥으로 여행 약속을 잡는 경우가 많아요.",
@@ -97,9 +109,9 @@
           description6: "평소 가고 싶었던 여행지의 할인 이벤트를 보고 굉장히 신나해요",
           description7: "같이 놀러 간 친구가 힘들어 보이면 응원을 해주며 마지막으로 한 번만 가자고 어떻게든 설득해요.",
           imageUrl: require("@/assets/images/ESFP.png"), // 예시 경로
-          style: "여행즉흥론자",
-          soulMate: "여행한정인싸(ISFJ)",
-          hateMate: "효율계획러(INTJ)"
+          style: "여행 즉흥론자",
+          soulMate: "여행 한정인싸(ISFJ)",
+          hateMate: "효율 계획러(INTJ)"
         },
         "ESTJ": {
           description: "여행을 가고자 확실하게 마음을 먹으면 엄청난 추진력으로 깔끔하게 여행 계획을 세워요.",
@@ -109,9 +121,9 @@
           description5: "예기치 못한 상황이 발생할 것을 대비하여 플랜B도 미리 계획해 둡니다.",
           description6: "이렇게까지 계획을 세우는 이유는 시간 낭비를 줄이고 돈 낭비를 줄이기 위해서입니다.",
           imageUrl: require("@/assets/images/ESTJ.png"), // 예시 경로
-          style: "엑셀마스터",
-          soulMate: "프로혼행러(ISTP)",
-          hateMate: "로맨틱여행파(INFP)"
+          style: "엑셀 마스터",
+          soulMate: "프로 혼행러(ISTP)",
+          hateMate: "로맨틱 여행파(INFP)"
         },
         "ESTP": {
           description: "친구들과 여행을 갈 때 각자 가고 싶은 여행지가 다를 경우중재를 하며 해결책을 마련해줍니다.",
@@ -121,9 +133,9 @@
           description5: "어느 여행지에 가서도 특유의 친화력으로 여행지 사람과 맞팔하거나 친하게 지내요.",
           description6: "그 여행지의 맛집에 방문하거나 신박한 아이템이 있으면 이걸로 장사하면 잘될 것 같은데 왜 안하지? 라고 생각하며 즐겨요.",
           imageUrl: require("@/assets/images/ESTP.png"), // 예시 경로
-          style: "프로플렉서",
-          soulMate: "프로엑셀러(ISTJ)",
-          hateMate: "감성여행자(INFJ)"
+          style: "프로 플렉서",
+          soulMate: "프로 엑셀러(ISTJ)",
+          hateMate: "감성 여행자(INFJ)"
         },
         "INFJ": {
           description: "숙소에 감성이 가득하며 돌아다니기보다 그 안에서 모든 걸 해결할 수 있는 여행이 좋아요.",
@@ -146,9 +158,9 @@
           description5: "다 같이 하는 여행도 좋지만 혼자 있길 좋아하고 혼자만의 시간이 필요해 혼자 떠나는 여행도 좋아해요.",
           description6: "여행 계획을 짤 때 완벽한 계획이길 원하지만, 너무 빈틈없는 꼼꼼한 여행보다 융통성이 있는 계획을 선호해요.",
           imageUrl: require("@/assets/images/INFP.png"), // 예시 경로
-          style: "로맨틱여행파",
-          soulMate: "프로여행러(ENFJ)",
-          hateMate: "프로핫스팟러(ESFJ)"
+          style: "로맨틱 여행파",
+          soulMate: "프로 여행러(ENFJ)",
+          hateMate: "프로 핫스팟러(ESFJ)"
         },
         "INTJ": {
           description: "남들이 다 가는 유명한 여행지보다 사람이 적고 조용한 여행을 선호해요.",
@@ -158,9 +170,9 @@
           description5: "계획이 디테일 할수록 본인이 가장 뿌듯해하며 같이 가는 사람들에게 정리한 파일을 공유하는 것도 좋아합니다.",
           description6: "다 같이 여행을 갈 때도 모두가 꼭 방문하고 싶은 메인 관광지를 선정해 장소를 취합하고 그 관광지의 주변 맛집과 쇼핑할 것 구경할 만한 곳을 찾아봐요.",
           imageUrl: require("@/assets/images/INTJ.png"), // 예시 경로
-          style: "효율계획러",
-          soulMate: "위기탈출넘버원(ENTP)",
-          hateMate: "여행즉흥론자(ESFP)"
+          style: "효율 계획러",
+          soulMate: "위기탈출 넘버원(ENTP)",
+          hateMate: "여행 즉흥론자(ESFP)"
         },
         "INTP": {
           description: "사람들에게 많이 알려지지 않은 여행 장소를 좋아합니다.",
@@ -170,9 +182,9 @@
           description5: "누군가 계획을 잡거나 스케줄을 알려주면 세부적인 것들을 정리해요. (맛집/카페/놀 것 찾기)",
           description6: "사람이 많이 없는 카페 같은 곳에서 그곳의 한적함을 느끼며, 나만의 감성 사진을 찍을 수 있는 곳으로 떠나고 싶어요.",
           imageUrl: require("@/assets/images/INTP.png"), // 예시 경로
-          style: "한적스팟러",
-          soulMate: "효율로보트(ENTJ)",
-          hateMate: "프로핫스팟러(ESFJ)"
+          style: "한적 스팟러",
+          soulMate: "효율 로보트(ENTJ)",
+          hateMate: "프로 핫스팟러(ESFJ)"
         },
         "ISFJ": {
           description: "평소에도 미리 가고 싶은 여행지, 숙소, 맛집 등이 메모장에 적혀있어요.",
@@ -182,9 +194,9 @@
           description5: "리적으로 편하다고 생각하는 친구들과 여행 가길 좋아 하며 그 친구들과 함께 할 땐 뜬금없이 친구들을 웃게 해요.",
           description6: "여행 짐을 챙길 때도 부족함 없이 '누군가는 쓰겠지' 라며 넉넉하게 짐을 챙겨요.",
           imageUrl: require("@/assets/images/ISFJ.png"), // 예시 경로
-          style: "여행한정인싸",
-          soulMate: "여행즉흥론자(ESFP)",
-          hateMate: "위기탈출넘버원(ENTP)"
+          style: "여행 한정인싸",
+          soulMate: "여행 즉흥론자(ESFP)",
+          hateMate: "위기탈출 넘버원(ENTP)"
         },
         "ISFP": {
           description: "스트레스를 받고 있을 때 여행을 떠나고 싶어 즉흥으로 친구들과 여행 약속을 잡아요.",
@@ -195,9 +207,9 @@
           description6: "여행을 하며 돌아다니는 것도 좋지만 나를 위한 힐링의 시간이기 때문에 너무 타이트한 여행은 힘들어요.",
           description7: "여행을 통해 여태껏 받아왔던 나의 스트레스를 풀어요.",
           imageUrl: require("@/assets/images/ISFP.png"), // 예시 경로
-          style: "힐링여행러",
-          soulMate: "프로핫스팟러(ESFJ)",
-          hateMate: "효율로보트(ENTJ)"
+          style: "힐링 여행러",
+          soulMate: "프로 핫스팟러(ESFJ)",
+          hateMate: "효율 로보트(ENTJ)"
         },
         "ISTJ": {
           description: "여행을 간다고 하면 누가 시키지 않아도 스스로 알아서 일정을 계획하고 정리해요.",
@@ -207,9 +219,9 @@
           description5: "숙소나 그 여행지의 할인 방법을 정리하며 최고의 가성비 여행을 준비해요.",
           description6: "친구들과 여행에 갈 때도 계획을 세우며 블로그, 카페 등 후기와 정보들을 다 찾아봐요.",
           imageUrl: require("@/assets/images/ISTJ.png"), // 예시 경로
-          style: "프로엑셀러",
-          soulMate: "프로플렉서(ESTP)",
-          hateMate: "프로여행러(ENFJ)"
+          style: "프로 엑셀러",
+          soulMate: "프로 플렉서(ESTP)",
+          hateMate: "프로 여행러(ENFJ)"
         },
         "ISTP": {
           description: "평소에는 내성적이지만 여행할 땐 대담한 모습을 보여줘요.",
@@ -220,9 +232,9 @@
           description6: "남들이 다하는 여행에 만족감을 느끼기보다 나만의 여행 속 즐거움이 따로 있어요.",
           description7: "여행을 좋아하긴 하지만 집에서 조용히 혼자만의 시간을 보내는 것을 더 좋아해요.",
           imageUrl: require("@/assets/images/ISTP.png"), // 예시 경로
-          style: "프로혼행러",
-          soulMate: "효율로보트(ENTJ)",
-          hateMate: "프로여행러(ENFJ)"
+          style: "프로 혼행러",
+          soulMate: "효율 로보트(ENTJ)",
+          hateMate: "프로 여행러(ENFJ)"
         },
         // 추가 유형...
       }
@@ -253,30 +265,103 @@
    @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap');
  
  .survey-container {
+    font-family: "Nanum Gothic", sans-serif;
+    font-style: normal;
+    font-weight: bold; /* 글자 두께 */
     /*background-image: url('@/assets/images/travel2.jpg'); /* 여기에 이미지 파일의 경로를 넣으세요 */
     background-size: contain; /* 이미지가 컨테이너를 꽉 채우도록 설정 */
     background-position: center; /* 이미지가 컨테이너의 중앙에 위치하도록 설정 */
     background-repeat: no-repeat; /* 이미지가 반복되지 않도록 설정 */
     background-color: #8EDFFD;
-    height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     text-align: center;
-    margin-top: 50px;
+    margin-top: 80px;
   }
 
   .mbtiType {
-    font-family: "Nanum Gothic", sans-serif;
-    font-style: normal;
     color: #ffec99; /* 글자 색상 */
-    font-size: 5rem; /* 글자 크기 */
-    font-weight: bold; /* 글자 두께 */
+    font-size: 3rem; /* 글자 크기 */
     text-shadow: 
     3px 3px 0px #ffd700, /* 글자의 오른쪽과 아래쪽에 황금색 그림자 */
     -1px -1px 0 #ffd700, /* 글자의 왼쪽과 위쪽에 황금색 그림자 */
-    3px 3px 15px rgba(0, 0, 0, 0.2); /* 글자 주변에 부드러운 검은색 그림자로 깊이감 추가 */
+    3px 3px 10px rgba(0, 0, 0, 0.1); /* 글자 주변에 부드러운 검은색 그림자로 깊이감 추가 */
+    margin-bottom: 20px;
+  }
+  .mbtiImage{
+    height: 300px;
+    width: 300px;
+    margin-bottom: 50px;
+    margin-top: 60px;
+  }
+  .mbtiProfile-description{
+    text-align: start;
+  }
+  .mbtiProfile-description p {
+    margin-bottom: 30px;
+  }
+
+  .styleModal {
+    background-color: #ffffff;
+    padding: 20px 30px;
+    border-radius: 8%;
+    max-width: 550px;
+     box-shadow: 
+    0px 4px 6px rgba(0, 0, 0, 0.1),    /* 부드러운 그림자 */
+    0px 1px 3px rgba(0, 0, 0, 0.08),   /* 작은 세부 그림자 */
+    0px 10px 20px rgba(0, 0, 0, 0.06); /* 큰 퍼진 그림자 */
+  }
+
+  .borderLine{
+    background: linear-gradient(#8EDFFD, #FFFFFF);
+    width: 650px;
+    padding-left: 50px;
+    box-shadow: 
+    5px 5px 15px rgba(58, 145, 195, 0.2),  /* 오른쪽 그림자 */
+    -5px 5px 15px rgba(58, 145, 195, 0.2); /* 왼쪽 그림자 */
+  }
+
+    .restart-button {
+    font-family: "Nanum Gothic", sans-serif;
+    font-style: normal;
+    padding: 1rem 2rem;
+    font-size: 1rem;
+    font-weight: bold;
+    color: #ffffff;
+    background-color: #8EDFFD;
+    border: none;
+    border-radius: 25px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+    transition: transform 0.3s ease;
+    width: 400px;
+    margin-top: 50px;
+    margin-bottom: 25px;
+    margin-right: 60px;
+  }
+
+      .home-button {
+    font-family: "Nanum Gothic", sans-serif;
+    font-style: normal;
+    padding: 1rem 2rem;
+    font-size: 1rem;
+    font-weight: bold;
+    color: #ffffff;
+    background-color: #8EDFFD;
+    border: none;
+    border-radius: 25px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+    transition: transform 0.3s ease;
+    width: 400px;
+    margin-bottom: 25px;
+    margin-right: 60px;
+  }
+  
+  .start-button:hover {
+    transform: scale(1.05);
   }
   </style>
   
