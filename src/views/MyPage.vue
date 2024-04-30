b864d953c8bdc46d42d1a27727c3dd0db97b3984
Ruah Kim <kimhk0315@gmail.com>
2024-04-30 16:28:22 +0900

    🎨 회원탈퇴 버튼

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,98 +1,99 @@
 <template>
   <div>
-  <div class="divider" />
-  <div class="info-container">
-    <div class="member">
-      <div class="mypage-maintext">마이 페이지</div>
-      <div class="mypage-content-row">
-        <div class="member-image">
-          <div class="file-upload-wrapper">
-            <div class="image-upload-preview" v-if="profileImageSrc">
-              <img :src="profileImageSrc" class="profile-image" />
+    <div class="divider" />
+    <div class="info-container">
+      <div class="member">
+        <div class="mypage-maintext">마이 페이지</div>
+        <div class="mypage-content-row">
+          <div class="member-image">
+            <div class="file-upload-wrapper">
+              <div class="image-upload-preview" v-if="profileImageSrc">
+                <img :src="profileImageSrc" class="profile-image" />
 
-              <!-- 이모티콘은 적절한 아이콘으로 교체 가능 -->
-            </div>
-            <div v-else class="image-upload-placeholder"></div>
-            <input
-              type="file"
-              id="profileImage"
-              ref="profileImage"
-              @change="handleFileChange"
-              style="display: none"
-            />
-          </div>
-        </div>
-        <div class="member-content">
-          <div class="info-group">
-            <label for="email">이메일</label>
-            <div class="member-info" id="name">{{ memberDetails.email }}</div>
-          </div>
-          <div class="info-group">
-            <label for="name">이 름</label>
-            <div class="member-info" id="name">
-              {{ memberDetails.userName }}
+                <!-- 이모티콘은 적절한 아이콘으로 교체 가능 -->
+              </div>
+              <div v-else class="image-upload-placeholder"></div>
+              <input
+                type="file"
+                id="profileImage"
+                ref="profileImage"
+                @change="handleFileChange"
+                style="display: none"
+              />
             </div>
           </div>
-          <div
-            v-if="currentPassword !== null && currentPassword !== ''"
-            class="info-group"
-          >
-            <label for="pw">비밀번호</label>
-            <div class="member-info">
-              <div @click="openPwModal" type="button" id="pw" class="pw">
-                비밀번호 변경
+          <div class="member-content">
+            <div class="info-group">
+              <label for="email">이메일</label>
+              <div class="member-info" id="name">{{ memberDetails.email }}</div>
+            </div>
+            <div class="info-group">
+              <label for="name">이 름</label>
+              <div class="member-info" id="name">
+                {{ memberDetails.userName }}
               </div>
             </div>
-          </div>
-          <PasswordChangeModal
-            :memberDetails="memberDetails"
-            :isChangingPw="isPwModalVisible"
-            @close="closePwModal"
-          />
-          <div class="info-group">
-            <label for="age">나 이</label>
-            <div class="member-info" id="age">{{ memberDetails.age }}</div>
-          </div>
-          <div class="info-group">
-            <label for="gender">성 별</label>
-            <div class="member-info" id="gender">
-              {{
-                memberDetails.gender === "F"
-                  ? "여"
-                  : memberDetails.gender === "M"
-                  ? "남"
-                  : "정보 없음"
-              }}
+            <div
+              v-if="currentPassword !== null && currentPassword !== ''"
+              class="info-group"
+            >
+              <label for="pw">비밀번호</label>
+              <div class="member-info">
+                <div @click="openPwModal" type="button" id="pw" class="pw">
+                  비밀번호 변경
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="info-group">
-            <label for="address">주 소</label>
-            <div class="member-info" id="address">
-              {{ memberDetails.address }}
+            <PasswordChangeModal
+              :memberDetails="memberDetails"
+              :isChangingPw="isPwModalVisible"
+              @close="closePwModal"
+            />
+            <div class="info-group">
+              <label for="age">나 이</label>
+              <div class="member-info" id="age">{{ memberDetails.age }}</div>
             </div>
-          </div>
-          <div class="info-group">
-            <label for="findByAnswer">가고싶은 여행지는?</label>
-            <div class="member-info" id="findByAnswer">
-              {{ memberDetails.findByAnswer }}
+            <div class="info-group">
+              <label for="gender">성 별</label>
+              <div class="member-info" id="gender">
+                {{
+                  memberDetails.gender === "F"
+                    ? "여"
+                    : memberDetails.gender === "M"
+                    ? "남"
+                    : "정보 없음"
+                }}
+              </div>
             </div>
-          </div>
-          <div class="info-group">
-            <label for="style">여행 스타일</label>
-            <div class="member-info" id="style">
-              {{ memberDetails.memberStyle }}
+            <div class="info-group">
+              <label for="address">주 소</label>
+              <div class="member-info" id="address">
+                {{ memberDetails.address }}
+              </div>
+            </div>
+            <div class="info-group">
+              <label for="findByAnswer">가고싶은 여행지는?</label>
+              <div class="member-info" id="findByAnswer">
+                {{ memberDetails.findByAnswer }}
+              </div>
+            </div>
+            <div class="info-group">
+              <label for="style">여행 스타일</label>
+              <div class="member-info" id="style">
+                {{ memberDetails.memberStyle }}
+              </div>
+              <div @click="goToStyle" class="goToStyle">스타일 변경</div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="buttons">
-        <button type="button" class="goToModify" @click="goToModifyPage">
-          내 정보 수정하러 가기
-        </button>
+        <div class="buttons">
+          <button type="button" class="goToModify" @click="goToModifyPage">
+            내 정보 수정하러 가기
+          </button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -114,6 +115,9 @@ export default {
     this.fetchUserDetail();
   },
   methods: {
+    goToStyle() {
+      this.$router.push("/travel");
+    },
     goToModifyPage() {
       this.$router.push("/modify-member");
     },
